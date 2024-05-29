eb317d01e322e5a95029d35e4fa4870bc090500b
Rhamseys Garcia <rhamseysgarcia@MacBook-Pro.lan>
2024-05-29 13:10:16 -0700

    upload client

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import { BrowserRouter as Router } from 'react-router-dom';
-//npm install react-router-dom
-import reportWebVitals from './reportWebVitals';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import { HelmetProvider } from 'react-helmet-async';
+import { Provider } from 'react-redux';
+import store from './store.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.jsx';
+import reportWebVitals from  './reportWebVitals.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename='/'>
-    <App />
-    </Router>
+    <HelmetProvider>
+    <Provider store={store}>
+      <PayPalScriptProvider>
+        <Router>
+          <App/>
+        </Router>
+      </PayPalScriptProvider>
+    </Provider>
+    </HelmetProvider>
   </React.StrictMode>
 );
 
 reportWebVitals();
+
+
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5001', // Your API server address
+        changeOrigin: true,
+        rewrite: (path) => path.startsWith('/api') ? path : '/api' + path, // Adjusted path replacement
+      },
+    },
+  },
+  build: {
+    outDir: 'build', // Specify the output directory as "build"
+  },
+});
